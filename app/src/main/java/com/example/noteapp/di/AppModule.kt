6ddb035717efeb4e6e922915e6385524af74cb5c
Usman <muhammadusman19375@gmail.com>
2024-01-13 20:21:13 +0500

    first commit

---
+++ app/src/main/java/com/example/noteapp/di/AppModule.kt
@@ -0,0 +1,50 @@
+package com.example.noteapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.noteapp.feature_note.data.data_source.NoteDatabase
+import com.example.noteapp.feature_note.data.repository.NoteRepositoryImpl
+import com.example.noteapp.feature_note.domain.repository.NoteRepository
+import com.example.noteapp.feature_note.domain.use_case.AddNote
+import com.example.noteapp.feature_note.domain.use_case.DeleteNote
+import com.example.noteapp.feature_note.domain.use_case.GetNote
+import com.example.noteapp.feature_note.domain.use_case.GetNotes
+import com.example.noteapp.feature_note.domain.use_case.NoteUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(@ApplicationContext context: Context): NoteDatabase {
+        return Room.databaseBuilder(
+            context,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao())
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNotes = GetNotes(repository),
+            deleteNote = DeleteNote(repository),
+            addNote = AddNote(repository),
+            getNote = GetNote(repository)
+        )
+    }
+}
---
+++ app/src/main/java/com/example/noteapp/ui/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.example.noteapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+// System Color
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+//Custom Color
+val DarkGray = Color(0xFF202020)
+val LightBlue = Color(0xFFD7E8DE)
+
+val RedOrange = Color(0xffffab91)
+val RedPink = Color(0xfff48fb1)
+val BabyBlue = Color(0xff81deea)
+val Violet = Color(0xffcf94da)
+val LightGreen = Color(0xffe7ed9b)
